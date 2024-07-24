6737766e0c4f3aa86a5dc0e4664983206f770f50
Sudhanshu Abhishek <sudhanshu.abhishek@ltimindtree.com>
2024-07-24 16:55:41 +0000

    updated

--- cloud-run-skeleton/content/spanner.tf
+++ cloud-run-skeleton/content/spanner.tf
@@ -2,7 +2,7 @@ resource "google_spanner_database" "database" {
   instance = "appdb"
   name     = var.db_name
   ddl = [
-    "CREATE TABLE tasks ( id STRING(36) DEFAULT (GENERATE_UUID()), title STRING(300), status STRING(15), ) PRIMARY KEY (id);",
+    "CREATE TABLE tasks ( id STRING(36) DEFAULT (GENERATE_UUID()), title STRING(300), status STRING(15), ) PRIMARY KEY (id)",
   ]
   deletion_protection = false
 }
