eb37852ea80a393a8d5f9e42bb387a764e6c952a
Sudhanshu Abhishek <sudhanshu.abhishek@ltimindtree.com>
2024-08-05 13:49:44 +0000

    updated cloud run name

--- cloud-run-skeleton/content/cloud-run.tf
+++ cloud-run-skeleton/content/cloud-run.tf
@@ -22,7 +22,7 @@ resource "google_cloud_run_v2_service_iam_policy" "noauth" {
 
 resource "google_cloud_run_v2_service" "default" {
   project  = var.project_id
-  name     = "idp-${var.app_name}"
+  name     = "${var.app_name}"
   location = var.region
   ingress  = "INGRESS_TRAFFIC_ALL"
 
