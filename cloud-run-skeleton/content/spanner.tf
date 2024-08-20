3fa91813cb8055735166bad1377f9ed93dcba62b
Jinal Desai <58817990+jinaldesailive@users.noreply.github.com>
2024-08-20 11:26:01 +0100

    updated

--- cloud-run-skeleton/content/spanner.tf
+++ cloud-run-skeleton/content/spanner.tf
@@ -1,6 +1,6 @@
 resource "google_spanner_database" "database" {
   instance = "appdb"
-  name     = var.db_name
+  name     = lower("${var.db_name}")
   ddl = [
     "CREATE TABLE tasks ( id STRING(36) DEFAULT (GENERATE_UUID()), title STRING(300), status STRING(15), ) PRIMARY KEY (id)",
   ]
